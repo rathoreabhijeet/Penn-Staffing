639ffe6ee996fefaafb6ea715b2bb79e47bffa9f
Abhijeet Rathore <abhijeetrathoreiitd@gmail.com>
2017-02-15 18:11:01 +0530

    event page changed

--- www/js/controllers/eventDetailsController.js
+++ www/js/controllers/eventDetailsController.js
@@ -1,21 +1,12 @@
 var reloadpage = false;
 var configreload = {};
 angular.module('starter')
-    .controller('EventDetailCtrl', function ($scope, $stateParams, MyServices, $location, $ionicLoading, $ionicSlideBoxDelegate, $ionicModal) {
-        ;
-        // $scope.showloading = function () {
-        //     $ionicLoading.show({
-        //         template: '<ion-spinner class="spinner-positive"></ion-spinner>'
-        //     });
-        //     $timeout(function () {
-        //         $ionicLoading.hide();
-        //     }, 5000);
-        // };
+    .controller('EventDetailCtrl', function ($scope, $stateParams, MyServices, $location, $ionicLoading, 
+                                             $ionicSlideBoxDelegate, $ionicModal,$state, $rootScope, EventsInfo, $localForage) {
 
-        $scope.msg = "Loading...";
         $scope.video = {};
-        $scope.image = {};
-
+        $scope.image = {}
+        var categoryIndex = 0;
 
         var init = function () {
             return $ionicModal.fromTemplateUrl('templates/appView/modal-video.html', {
@@ -58,24 +49,134 @@ angular.module('starter')
         };
 
         $scope.id = $stateParams.id;
-        var getsingleeventscallback = function (data, status) {
-            if (data == "") {
-                $scope.msg = "No data found";
-                addanalytics("Event detail page");
-            } else {
-                $scope.msg = "";
-                addanalytics(data.title);
+
+        function fetchEventDetailData() {
+            $ionicLoading.show({
+                template: '<ion-spinner class="spinner-positive"></ion-spinner>'
+            });
+
+            categoryIndex = _.findIndex(EventsInfo.data, function (n) {
+                return n.id == $stateParams.id;
+            })
+            $scope.eventdetail = EventsInfo.data[categoryIndex];
+
+
+            console.log(EventsInfo.data);
+            console.log(categoryIndex);
+            if (EventsInfo.data[categoryIndex].detail) {
+                console.log('service contains the gallery');
+                //binding local variable to service
+                $scope.eventdetail.detail = EventsInfo.data[categoryIndex].detail;
+                $ionicLoading.hide();
             }
-            if (data.eventimages && data.eventimages.length > 0) {
-                data.eventimages = _.chunk(data.eventimages, 2);
+            else {
+                console.log('service does not contain the gallery');
+                $localForage.getItem('events').then(function (data) {
+                    if (data != null) {
+                        console.log('forage exists');
+                        console.log(data);
+                        var index = _.findIndex(data, function (n) {
+                            return n.id == $stateParams.id;
+                        })
+                        if (index != -1 && data[index].detail) {
+                            console.log('current gallery exists in forage');
+                            $scope.eventdetail.detail = angular.copy(data[index].detail);
+                            $ionicLoading.hide();
+                        }
+                        else {
+                            console.log('current gallery no exist in forage');
+                            loadEventDetail(1);
+                        }
+                    }
+                    else {
+                        console.log('forage no exists');
+                        loadEventDetail(2);
+                    }
+                })
             }
-            if (data.eventvideos && data.eventvideos.length > 0) {
-                data.eventvideos = _.chunk(data.eventvideos, 2);
-            }
-            $scope.eventdetail = data;
-            $ionicSlideBoxDelegate.update();
+
         }
-        MyServices.getsingleevents($stateParams.id, getsingleeventscallback, function (err) {
-            $location.url("/access/offline");
-        })
+
+        function loadEventDetail(index){
+            $ionicLoading.show({
+                template: '<ion-spinner class="spinner-positive"></ion-spinner>'
+            });
+            MyServices.getsingleevents($stateParams.id, function(data){
+                if (data == "") {
+                    $scope.msg = "No data found";
+                    addanalytics("Event detail page");
+                } else {
+                    $scope.msg = "";
+                    addanalytics(data.title);
+                }
+                if (data.eventimages && data.eventimages.length > 0) {
+                    data.eventimages = _.chunk(data.eventimages, 2);
+                }
+                if (data.eventvideos && data.eventvideos.length > 0) {
+                    data.eventvideos = _.chunk(data.eventvideos, 2);
+                }
+                $scope.eventdetail.detail = angular.copy(data);
+                $ionicSlideBoxDelegate.update();
+
+
+                if (index == 1) {
+                    $localForage.getItem('events').then(function (forageData) {
+                        var forageIndex = _.findIndex(forageData, function (n) {
+                            return n.id == $stateParams.id;
+                        })
+                        forageData[forageIndex].detail = $scope.eventdetail.detail;
+                        $localForage.setItem('events', forageData);
+                        console.log(forageData);
+                        console.log('gallery saved in existing forage');
+                    })
+
+                }
+                if (index == 2) {
+                    var array = [];
+                    array.push(EventsInfo.data[categoryIndex]);
+                    $localForage.setItem('events', array);
+                    console.log('gallery saved in new forage');
+                }
+
+                $ionicLoading.hide();
+            }, function (err) {
+                $location.url("/access/offline");
+            })
+        }
+
+        fetchEventDetailData();
+
+        $scope.footerLink = function(links){
+            switch (links.linktype) {
+                case '3':
+                    links.typeid = links.event;
+                    break;
+                case '6':
+                    links.typeid = links.gallery;
+                    break;
+                case '8':
+                    links.typeid = links.video;
+                    break;
+                case '10':
+                    links.typeid = links.blog;
+                    break;
+                case '2':
+                    links.typeid = links.article;
+                    break;
+                default:
+                    links.typeid = 0;
+
+            }
+            if(links.name=="Phone Call"){
+                window.open('tel:' + ('+1' + $rootScope.phoneNumber), '_system');
+            }
+            else if (links.name == "Home") {
+                $state.go("app." + $rootScope.homeLink);
+
+            }
+            else {
+                $state.go("app." + links.linktypelink, {id: links.typeid, name: links.name});
+            }
+        }
+
     })
--- www/js/controllers/eventsController.js
+++ www/js/controllers/eventsController.js
@@ -1,22 +1,42 @@
 var reloadpage = false;
 var configreload = {};
 angular.module('starter')
-    .controller('EventsCtrl', function ($scope, MyServices, $location, $ionicLoading, $filter, EventsInfo) {
+    .controller('EventsCtrl', function ($scope, MyServices, $location, $ionicLoading,
+                                        $filter, EventsInfo, $state, $rootScope, $localForage) {
         addanalytics("Event page");
-        ;
+        // configreload.onallpage();
         $ionicLoading.show();
         $scope.pageno = 1;
         $scope.events = [];
         $scope.keepscrolling = true;
-        $scope.msg = "Loading....";
-        // $scope.showloading = function () {
-        //     $ionicLoading.show({
-        //         template: '<ion-spinner class="spinner-positive"></ion-spinner>'
-        //     });
-        //     $timeout(function () {
-        //         $ionicLoading.hide();
-        //     }, 5000);
-        // };
+        $scope.events = EventsInfo.data;
+
+        function fetchEventData() {
+            if (EventsInfo.data.length == 0) {
+                $ionicLoading.show({
+                    template: '<ion-spinner class="spinner-positive"></ion-spinner>'
+                });
+                console.log('service empty');
+                $localForage.getItem('events').then(function (data) {
+                    if (data != null) {
+                        console.log('forage exists');
+                        console.log(data);
+                        _.each(data,function(n){
+                            $scope.events.push(n);
+                        })
+                        $ionicLoading.hide();
+                    }
+                    else {
+                        console.log('forage no exists');
+                        loadEvents();
+                    }
+                })
+            }
+            else {
+                console.log('service filled');
+                $ionicLoading.hide();
+            }
+        }
 
         $scope.share = function (item) {
             var data = {};
@@ -26,9 +46,11 @@ angular.module('starter')
             window.plugins.socialsharing.share('Checkout "' + item.title + '" starting on ' + data.startdate + ', ' + data.starttime, null, data.image + 'At ' + item.venue);
         }
 
-        $scope.loadevents = function (pageno) {
-            if (EventsInfo.data.length == 0) {
-                MyServices.getallevents(pageno, function (data) {
+        function loadEvents () {
+            $ionicLoading.show({
+                template: '<ion-spinner class="spinner-positive"></ion-spinner>'
+            });
+                MyServices.getallevents(1, function (data) {
                     $ionicLoading.hide();
                     _.each(data.queryresult, function (n) {
                         $scope.events.push(n);
@@ -43,29 +65,56 @@ angular.module('starter')
                     if (data.queryresult.length == 0) {
                         $scope.keepscrolling = false;
                     }
-                    EventsInfo.data = $scope.events;
                     console.log($scope.events);
+                    $localForage.setItem('events', $scope.events);
                     $ionicLoading.hide();
                 }, function (err) {
                     $location.url("/access/offline");
                 })
-            }
-            else {
-                $scope.events = EventsInfo.data;
-                $ionicLoading.hide();
-            }
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-            $scope.$broadcast('scroll.refreshComplete');
         }
 
-        $scope.loadevents(1);
-
-        $scope.loadMorePolls = function () {
-            $scope.loadevents(++$scope.pageno);
-        }
+        fetchEventData();
 
         $scope.geteventdetails = function (id) {
             $location.url("app/eventdetail/" + id);
+        }
+
+        $scope.footerLink = function(links){
+            switch (links.linktype) {
+                case '3':
+                    links.typeid = links.event;
+                    break;
+                case '6':
+                    links.typeid = links.gallery;
+                    break;
+                case '8':
+                    links.typeid = links.video;
+                    break;
+                case '10':
+                    links.typeid = links.blog;
+                    break;
+                case '2':
+                    links.typeid = links.article;
+                    break;
+                default:
+                    links.typeid = 0;
+
+            }
+            if(links.name=="Phone Call"){
+                window.open('tel:' + ('+1' + $rootScope.phoneNumber), '_system');
+            }
+            else if (links.name == "Home") {
+                $state.go("app." + $rootScope.homeLink);
+
+            }
+            else {
+                $state.go("app." + links.linktypelink, {id: links.typeid, name: links.name});
+            }
+        }
+
+        $scope.refreshAllData = function () {
+            $scope.events.length = 0;
+            loadEvents();
         }
 
     })
